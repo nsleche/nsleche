23cfe078f00eb56fce062022b85e536534a7b679
Antonio Alves <antonioalvesdev@gmail.com>
2025-11-03 14:42:53 -0800

    Remove footer content for now

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='https://github.com/nsleche'
       className='link footer__link'
     >
-      Created By Antonio Alves
+    {/* GitHub */}
     </a>
   </footer>
 )
