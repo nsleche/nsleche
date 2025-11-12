203f08729aea7ccd2b272203e82506b7dbaa6127
Antonio Alves <antonioalvesdev@gmail.com>
2025-11-12 11:18:55 -0800

    Change homepage link to refer new domain

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://nsleche.github.io/cleanfolio',
+  homepage: 'https://nsleche.com',
   title: 'AA.',
 }
 
